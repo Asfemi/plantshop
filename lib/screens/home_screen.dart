dcbb014fa22ab390f90dd3018b7ef5aea3a4462e
Asfemi <pearlcstall@gmail.com>
2023-11-15 17:14:20 +0100

    search card worked on

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,43 +17,94 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Column(
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'welcome 🔥',
                       style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 18,
                           fontWeight: FontWeight.w500,
                           color: Colors.black),
                     ),
                     Text(
                       'Ayo Maselliene',
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 18,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
                   ],
                 ),
-                Card(
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  color: Colors.white,
-                  child: const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Icon(Icons.notifications_active_outlined),
-                  ),
+                IconCard(
+                  cardChild: Icon(Icons.notifications_active_outlined),
+                  cardColor: Colors.white,
                 ),
               ],
             ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Card(
+                    elevation: 0,
+                    shadowColor: kLightCardColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    color: Colors.white,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search,
+                              color: Colors.black.withOpacity(0.5)),
+                          hintText: '   Search',
+                          hintStyle:
+                              TextStyle(color: Colors.black.withOpacity(0.5))),
+                    ),
+                  ),
+                ),
+                const IconCard(
+                    cardChild: Icon(
+                      Icons.dashboard,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    cardColor: Colors.black)
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    required this.cardChild,
+    required this.cardColor,
+    super.key,
+  });
+
+  final Color cardColor;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      color: cardColor,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: cardChild,
       ),
     );
   }
