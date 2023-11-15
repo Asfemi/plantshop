2bd39105f515c150cb50af412546a538b37bc8dc
Asfemi <pearlcstall@gmail.com>
2023-11-15 12:52:45 +0100

    intial commit
    project setup and base components additions

---
+++ lib/screens/purchase_plant_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PurchasePlantScreen extends StatefulWidget {
+  const PurchasePlantScreen({super.key});
+
+  @override
+  State<PurchasePlantScreen> createState() => _PurchasePlantScreenState();
+}
+
+class _PurchasePlantScreenState extends State<PurchasePlantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
